2917c77dc0b536411fe455a5664b2c0e5c753b60
desyatkov93 <fun-444@bk.ru>
2015-02-16 02:10:44 +0500

    commit
    comments

--- script/Myleneature.js
+++ script/Myleneature.js
@@ -1,13 +1,25 @@
 $(document).ready(function()
 {
 (function(){
-$("body").append('<div id=\'MyleneatureWrap\'></div>');	
-$("#MyleneatureWrap").append('<div id=\'MyleneatureBlock\'></div>');	
-$("#MyleneatureBlock").append('<img class=\'MyleneatureImg\'>');	
-function ShowImg(St_img) 
+// добавляем в документ необходимые блоки для миниатюр
+$("body").append('<div id=\'MyleneatureWrap\'></div>');	// тёмное фиксированое поле, не даёт жмакать на другие элементы, при просмотре Милениатюры
+$("#MyleneatureWrap").append('<div id=\'MyleneatureBlock\'></div>'); // Белый поле, в центре которого будет изображение	
+$("#MyleneatureBlock").append('<img class=\'MyleneatureImg\'>'); // Изображение	
+
+
+// ФУНКЦИИ
+
+function activeWrap()  // Делает видимыми основные блоки милениатюры
+{
+	$("#MyleneatureWrap").css('display','block');
+	$("#MyleneatureBlock").css('display','block');
+	$(".MyleneatureImg").css('display','block');
+}
+
+function ShowImg(St_img)  // Определяет какое изображение надо выводить в милениатюру
 {
 var fullImg = $(St_img).attr('full');
-if(fullImg == undefined)
+if(fullImg == undefined) // Если атрибут full обьявлен, то выводим изображение, находящиеся по адресу из этого атрибута
 {
 var str = St_img.src;
 }
@@ -17,8 +29,16 @@ var str = fullImg;
 }
 $(".MyleneatureImg").attr('src',str);
 }
-	
-function hider()
+
+function removeScroll(){      // Убирает скролл
+	$("body").css('height','100%').css('width','100%').css('overflow','hidden');
+}
+
+function backScroll(){ // возвращает скролл
+	$("body").css('height','auto').css('width','auto').css('overflow','auto');
+}	
+
+function hider()  // прячет Милениатюры
 {
 	$(".MyleneatureImg").css('display','none');
 	$("#MyleneatureWrap").css('display','none');
@@ -27,22 +47,14 @@ function hider()
 	backScroll();
 }
 	
-function removeScroll(){
-	$("body").css('height','100%').css('width','100%').css('overflow','hidden');
-}
-function backScroll(){
-	$("body").css('height','auto').css('width','auto').css('overflow','auto');
-}
-function activeWrap()
+
+	
+	
+// ОБРАБОТЧИКИ
+
+$("img").click(function()  // делегируем все изображения документа
 {
-	$("#MyleneatureWrap").css('display','block');
-	$("#MyleneatureBlock").css('display','block');
-	$(".MyleneatureImg").css('display','block');
-}
-// delegation in the all images
-$("img").click(function()
-{
-	if($(this).attr('myleneature') == '')
+	if($(this).attr('myleneature') == '') // проверяем наличеие атрибута указывающего на изображение, как на объект милениатюры
 	{
 		activeWrap();
 		ShowImg(this);
@@ -50,16 +62,15 @@ $("img").click(function()
 			{
 				removeScroll();
 			}
-		
 	}
-}
-);
-// hide Myleneature 
+});
+
+// прячем милениатюру 
 $("#MyleneatureWrap").click(function(){
 hider();
 });
 
-// Stop bubbling
+// Останавливаем всплытие на изображении, что бы милениатюра не схлопывалась по клику на изображение 
 $(".MyleneatureImg")[0].onclick = function(event) {
   event = event || window.event; 
   if (event.stopPropagation) {
@@ -68,7 +79,5 @@ $(".MyleneatureImg")[0].onclick = function(event) {
     event.cancelBubble = true;
   }
 }
-
-
 })();
 });
